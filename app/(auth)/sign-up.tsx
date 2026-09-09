7c9223044bf55381d81b0595a7633a4433391210
haruvinia <75082071+haruvinia@users.noreply.github.com>
2026-09-09 16:47:40 +0000

    c

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,15 +1,125 @@
-import { Text, View } from "react-native";
+import Feather from "@expo/vector-icons/Feather";
+import { Image } from "expo-image";
+import { useRouter } from "expo-router";
+
+import React from "react";
+
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function SignUp() {
+  const router = useRouter();
+
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "#fff",
+        justifyContent: "space-between",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <View style={{ width: "100%" }}>
+        <TouchableOpacity
+          style={{
+            display: "flex",
+            width: "100%",
+            padding: 20,
+            flexDirection: "row",
+            gap: 10,
+            alignItems: "flex-start",
+          }}
+          onPress={() => router.push("/(auth)/sign-in")}
+        >
+          <Feather name="arrow-left" size={24} color="black" />
+          <Text style={{ fontSize: 18, fontWeight: "500" }}>Login</Text>
+        </TouchableOpacity>
+      </View>
+      <Image
+        source={require("@/assets/images/logo-unique.png")}
+        style={{ width: 200, height: 200, resizeMode: "contain" }}
+      />
+      <View
+        style={{
+          width: "100%",
+          borderTopLeftRadius: 50,
+          borderTopRightRadius: 50,
+          backgroundColor: "#7B2CBF",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: 40,
+          padding: 50,
+        }}
+      >
+        <Text style={{ fontSize: 20, color: "#fff", fontWeight: "500" }}>
+          Insira seus dados para realizar o cadastro.
+        </Text>
+        <TextInput
+          style={{
+            width: "85%",
+            backgroundColor: "#fff",
+            padding: 15,
+            borderRadius: 30,
+            fontSize: 16,
+            paddingLeft: 25,
+          }}
+          keyboardType="email-address"
+          placeholder="Insira seu Nome"
+          placeholderTextColor="#999999"
+        />
+        <TextInput
+          style={{
+            width: "85%",
+            backgroundColor: "#fff",
+            padding: 15,
+            borderRadius: 30,
+            fontSize: 16,
+            paddingLeft: 25,
+          }}
+          keyboardType="email-address"
+          placeholder="Insira seu e-mail"
+          placeholderTextColor="#999999"
+        />
+        <TextInput
+          style={{
+            width: "85%",
+            backgroundColor: "#fff",
+            padding: 15,
+            borderRadius: 30,
+            fontSize: 16,
+            paddingLeft: 25,
+          }}
+          keyboardType="visible-password"
+          placeholder="Insira sua senha"
+          placeholderTextColor="#999999"
+        />
+        <TextInput
+          style={{
+            width: "85%",
+            backgroundColor: "#fff",
+            padding: 15,
+            borderRadius: 30,
+            fontSize: 16,
+            paddingLeft: 25,
+          }}
+          keyboardType="visible-password"
+          placeholder="Confirme sua senha"
+          placeholderTextColor="#999999"
+        />
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#fff",
+            paddingVertical: 15,
+            borderRadius: 30,
+            width: "85%",
+          }}
+        >
+          <Text
+            style={{ fontSize: 16, fontWeight: "500", textAlign: "center" }}
+          >
+            Cadastrar
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- app/selectAccountType.tsx
+++ app/selectAccountType.tsx
@@ -1,36 +1,102 @@
 import { Image } from "expo-image";
+
 import { useRouter } from "expo-router";
+
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+
+import { Text, TouchableOpacity, View } from "react-native";
+
 export default function SelectAccountType() {
-  
   const router = useRouter();
+
   return (
     <View
       style={{
         flex: 1,
+
         alignItems: "center",
+
         backgroundColor: "#fff",
       }}
     >
-      <View style={{backgroundColor: "#fff", width: "100%", display: "flex", alignItems: "center", justifyContent: "center"}}>
+      <View
+        style={{
+          backgroundColor: "#fff",
+          width: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <Image
           source={require("@/assets/images/logo.png")}
-          style={{ width: 100, height: 100, resizeMode: "contain"}}
+          style={{ width: 100, height: 100, resizeMode: "contain" }}
         />
       </View>
-      <View style={{backgroundColor: "#fff", flex: 1, width: "100%", display: "flex", alignItems: "center", gap: 35, borderBottomEndRadius: 50}}>
-        <Image source={require("@/assets/images/selectTypeImage.png")} style={{ width: 200, height: 200}} />
-        <Text style={{fontSize: 35, fontFamily: "Inter, sans-serif", textAlign: "center", fontWeight: 500}}>Seja bem-vindo!</Text>
-        <Text style={{color: "#7B2CBF", fontFamily: "Inter, sans-serif", textAlign: "center", fontWeight: 400, fontSize: 25}} >Desapegar ficou mais fácil!</Text>
+      <View
+        style={{
+          backgroundColor: "#fff",
+          flex: 1,
+          width: "100%",
+          alignItems: "center",
+          gap: 35,
+          borderBottomRightRadius: 50,
+        }}
+      >
+        <Image
+          source={require("@/assets/images/selectTypeImage.png")}
+          style={{ width: 200, height: 200 }}
+        />
+        <Text style={{ fontSize: 35, textAlign: "center", fontWeight: "500" }}>
+          Seja bem-vindo!
+        </Text>
+        <Text
+          style={{
+            color: "#7B2CBF",
+            textAlign: "center",
+            fontWeight: "400",
+            fontSize: 25,
+          }}
+        >
+          Desapegar ficou mais fácil!
+        </Text>
       </View>
-      <View style={{width: "100%", borderTopLeftRadius: 50, backgroundColor:"#7B2CBF", padding: 20, display: "flex", alignItems: "center", justifyContent: "center", flex: 1}}>
-        <TouchableOpacity style = {{backgroundColor: "#fff", padding: 15, paddingInline: 80, borderRadius: 30}}>
-          <Text style={{fontSize: 15, fontWeight: 500, fontFamily: "Arial, sans-serif"}}>Vamos começar</Text>
+      <View
+        style={{
+          width: "100%",
+          borderTopLeftRadius: 50,
+          borderTopRightRadius: 50,
+          backgroundColor: "#7B2CBF",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: 15,
+          padding: 30,
+          height: 400
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#fff",
+            paddingVertical: 15,
+            paddingHorizontal: 80,
+            borderRadius: 30,
+          }}
+        >
+          <Text style={{ fontSize: 15, fontWeight: "500" }}>Vamos começar</Text>
         </TouchableOpacity>
-        <View style={{display: "flex", flexDirection: "row", alignItems: "center", columnGap: 5}}>
-          <Text>Já possui conta?</Text>
-          <TouchableOpacity>Fazer login.</TouchableOpacity>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 5,
+            marginTop: 15,
+          }}
+        >
+          <Text style={{ color: "#fff" }}>Já possui conta?</Text>
+          <TouchableOpacity onPress={() => router.push("/(auth)/sign-in")}>
+            <Text style={{ color: "#fff", fontWeight: "bold", color:"#FFC98B"}}>
+              Fazer login.
+            </Text>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
